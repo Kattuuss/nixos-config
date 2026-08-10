de3f47064f1c2e2099bf2839e3842ba08e5030a8
Kattuuss <alexvalverdetorres2006@gmail.com>
2026-08-10 15:38:26 +0200

    refactor: clean up config, fix git module syntax, update GNOME options

--- flake.nix
+++ flake.nix
@@ -1,35 +1,35 @@
 {
-	description = "NixOS Flake configuration with Home Manager";
-	
-	inputs = {
-		# Unstable branch
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  description = "NixOS Flake configuration with Home Manager";
 
-		home-manager = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
+  inputs = {
+    # Unstable branch
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-	outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-		nixosConfigurations = {
-			qemu = nixpkgs.lib.nixosSystem {
-				system = "x86_64-linux";
-				modules = [
-					./hosts/vm/qemu/hardware-configuration.nix
-					./hosts/vm/qemu/configuration.nix
+    home-manager = {
+      url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-					# Integrates Home Manager into NixOS
-					home-manager.nixosModules.home-manager
-					{
-						home-manager.useGlobalPkgs = true;
-						home-manager.useUserPackages = true;
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      qemu = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/vm/qemu/hardware-configuration.nix
+          ./hosts/vm/qemu/configuration.nix
 
-						# Imports user home.nix
-						home-manager.users.kattus = import ./home/home.nix;
-					}
-				];
-			};
-		};
-	};
+          # Integrates Home Manager into NixOS
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # Imports user home.nix
+            home-manager.users.kattus = import ./home/home.nix;
+          }
+        ];
+      };
+    };
+  };
 }
